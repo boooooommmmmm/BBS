bf0d39e3dd3f257afe71c991d74a54d1262b1e43
Shuyan Hu <hushuyan88@gmail.com>
2019-01-27 19:23:17 +0800

    init

---
+++ upload/data/sysdata/cache_pluginsetting.php
@@ -0,0 +1,7 @@
+<?php
+//Discuz! cache file, DO NOT modify me!
+//Identify: 116faf14f214b673992db60e2bc7a5bb
+
+$pluginsetting = array (
+);
+?>
---
+++ upload/uc_server/data/config.inc.php
@@ -0,0 +1,18 @@
+<?php 
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '002682');
+define('UC_DBNAME', 'ultrax');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', 'pre_ucenter_');
+define('UC_COOKIEPATH', '/');
+define('UC_COOKIEDOMAIN', '');
+define('UC_DBCONNECT', 0);
+define('UC_CHARSET', 'utf-8');
+define('UC_FOUNDERPW', 'e3fadf8965f18968ff65ce159e083502');
+define('UC_FOUNDERSALT', 'U2b2G4');
+define('UC_KEY', 'z2j2S4re40vcN8e2j36a71r5d2m9Tfnf79V6J255gfT4p8scQ3a2Y0P6tehaR6aa');
+define('UC_SITEID', 'm2A2a4vee0tcx852y3ha616552F9vfof19O6H2y5wfx468Dch3t2q0i64euaC6oa');
+define('UC_MYKEY', '12H2M42eP0GcB8m2b3AaM1a5f2g9Pf7fy9z6i2a5yf04k8mci3n2S0h6heAaZ6Ja');
+define('UC_DEBUG', false);
+define('UC_PPP', 20);
